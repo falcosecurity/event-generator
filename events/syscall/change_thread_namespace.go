0f8ca21b05f7a71cbd0be781017032597f629691
Leonardo Grasso <me@leonardograsso.com>
2020-04-09 18:02:57 +0200

    new(events/syscall): change thread namespace
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

---
+++ events/syscall/change_thread_namespace.go
@@ -0,0 +1,20 @@
+// +build linux
+
+package syscall
+
+import (
+	"golang.org/x/sys/unix"
+
+	"github.com/falcosecurity/event-generator/events"
+)
+
+var _ = events.Register(ChangeThreadNamespace)
+
+func ChangeThreadNamespace(h events.Helper) error {
+	// It doesn't matter that the arguments to Setns are
+	// bogus. It's the attempt to call it that will trigger the
+	// rule.
+	h.Log().Debug("does not result in a falco notification in containers, unless container run with --privileged or --security-opt seccomp=unconfined")
+	unix.Setns(0, 0)
+	return nil
+}
