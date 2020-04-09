0526bb9877e39dbbd3dc78ddf50feff870ae5634
Leonardo Grasso <me@leonardograsso.com>
2020-04-09 17:42:20 +0200

    update(events): add Cleanup to the Helper interface
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- events/interfaces.go
+++ events/interfaces.go
@@ -5,9 +5,21 @@ import (
 	"k8s.io/cli-runtime/pkg/resource"
 )
 
+// A Helper is passed to an Action as argument.
 type Helper interface {
+
+	// Log returns an intermediate logger.Entry
+	// that already contains default fields for the current action.
 	Log() *logger.Entry
+
+	// Cleanup registers a function to be called when the action complete or later.
+	// Cleanup functions registered from within the same action will be called in last added,
+	// first called order.
+	Cleanup(f func(), args ...interface{})
+
+	// ResourceBuilder returns a k8s' resource.Builder.
 	ResourceBuilder() *resource.Builder
 }
 
+// An Action triggers an event.
 type Action func(Helper) error
