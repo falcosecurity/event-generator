547a1b33254a5fd4089d10b248ed26f032be75d3
Leonardo Grasso <me@leonardograsso.com>
2020-04-09 20:16:02 +0200

    new(events/syscall): exec ls
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

---
+++ events/syscall/exec_ls.go
@@ -0,0 +1,13 @@
+package syscall
+
+import (
+	"os/exec"
+
+	"github.com/falcosecurity/event-generator/events"
+)
+
+var _ = events.Register(ExecLs)
+
+func ExecLs(h events.Helper) error {
+	return exec.Command("/bin/ls").Run()
+}
