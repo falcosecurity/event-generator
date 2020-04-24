ae981bec8534a5d80aaada365d9aa1c045e1f1df
Leonardo Grasso <me@leonardograsso.com>
2020-04-24 10:47:19 +0200

    fix(syscall): isolate NonSudoSetuid in a child process
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- events/syscall/non_sudo_setuid.go
+++ events/syscall/non_sudo_setuid.go
@@ -9,11 +9,15 @@ import (
 var _ = events.Register(NonSudoSetuid)
 
 func NonSudoSetuid(h events.Helper) error {
-	h.Log().Debug("first setuid to something non-root, then try to setuid back to root")
-	if err := becameUser(h, "daemon"); err != nil {
-		return err
+	if h.Spawned() {
+		h.Log().Debug("first setuid to something non-root, then try to setuid back to root")
+		if err := becameUser(h, "daemon"); err != nil {
+			return err
+		}
+		err := becameUser(h, "root")
+		h.Log().WithError(err).Debug("ignore root setuid error")
+		return nil
+	} else {
+		return h.SpawnAs("child", "syscall.NonSudoSetuid")
 	}
-	err := becameUser(h, "root")
-	h.Log().WithError(err).Debug("ignore root setuid error")
-	return nil
 }
