701c8ed6aa131d81e40214822fa2df0aa75edcd1
Spencer Krum <nibz@spencerkrum.com>
2020-05-01 21:32:29 +0000

    new(events/syscall): add cronjob event
    Note that we disable cronjob events in the default falco rules so those
    will need to be turned on to see this event. Grep for
    'consider_all_cron_jobs' in falco_rules.yaml
    
    Signed-off-by: Spencer Krum <nibz@spencerkrum.com>
    
    fix(events/syscall): Use consistent naming and LookPath
    
    Co-Authored-By: Leonardo Grasso <me@leonardograsso.com>
    Signed-off-by: Spencer Krum <nibz@spencerkrum.com>

---
+++ events/syscall/schedule_cron_jobs.go
@@ -0,0 +1,32 @@
+// +build linux
+
+package syscall
+
+import (
+	"github.com/falcosecurity/event-generator/events"
+
+	"os/exec"
+)
+
+var _ = events.Register(ScheduleCronJobs)
+
+func ScheduleCronJobs(h events.Helper) error {
+	// This just lists crons, but sufficies to trigger the event
+	// Cron detection is not enabled by default, see `consider_all_cron_jobs` in rules.yaml
+
+	path, err := exec.LookPath("crontab")
+	if err != nil {
+		// if we don't have a crontab, just bail
+		h.Log().WithError(err).Debug("crontab utility not found in path")
+		return nil
+	}
+	cmd := exec.Command(path, "-l")
+	err = cmd.Run()
+
+	// silently ignore crontab exit status 1
+	if exitErr, isExitErr := err.(*exec.ExitError); isExitErr {
+		h.Log().WithError(exitErr).Debug("silently ignore exit status")
+		return nil
+	}
+	return err
+}
