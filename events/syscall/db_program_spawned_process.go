8fddb2b6cf2acceb451cf80ae1edcb4cf719a8dc
Leonardo Grasso <me@leonardograsso.com>
2020-06-04 19:08:06 +0200

    update(events/syscall): action renamed to `DbProgramSpawnedProcess`
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- events/syscall/db_program_spawned_process.go
+++ events/syscall/db_program_spawned_process.go
@@ -5,8 +5,8 @@ import (
 	_ "github.com/falcosecurity/event-generator/events/helper"
 )
 
-var _ = events.Register(DbProgramSpawnProcess)
+var _ = events.Register(DbProgramSpawnedProcess)
 
-func DbProgramSpawnProcess(h events.Helper) error {
+func DbProgramSpawnedProcess(h events.Helper) error {
 	return h.SpawnAs("mysqld", "helper.ExecLs")
 }
