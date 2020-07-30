5c00d92c188ed7dd45b8737e2a77cd3d07935b82
Leonardo Grasso <me@leonardograsso.com>
2020-07-30 12:14:26 +0200

    chore(pkg/counter): improved statistics
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- pkg/counter/stats.go
+++ pkg/counter/stats.go
@@ -2,32 +2,36 @@ package counter
 
 import (
 	"strconv"
+	"sync/atomic"
 	"time"
 
 	"github.com/dustin/go-humanize"
 	logger "github.com/sirupsen/logrus"
 )
 
-type Stats struct {
+type stats struct {
 	Action     string
 	Expected   uint64
 	Actual     uint64
 	Percentage int
 }
 
-func (c *Counter) logStats() {
-	if c.proc != nil {
-		ps := c.procStats()
-		c.log.WithFields(logger.Fields(ps)).Info("proc stats")
+func (c *Counter) statsByAction(n string) *stats {
+	stats := &stats{
+		Action: n,
 	}
-	for _, n := range c.actions {
-		s := c.Stats(n)
-		logEntry := c.log.WithField("expected", s.Expected).WithField("actual", s.Actual)
-		logEntry.Infof("%s (%d%% lost)", s.Action, 100-s.Percentage)
+	if s, ok := c.list[n]; ok {
+		stats.Expected, stats.Actual = atomic.LoadUint64(&s.expected), atomic.LoadUint64(&s.actual)
 	}
+
+	if stats.Expected > 0 {
+		stats.Percentage = int(100 * stats.Actual / stats.Expected)
+	}
+
+	return stats
 }
 
-func (c *Counter) procStats() (stats map[string]interface{}) {
+func (c *Counter) globalStats() (stats map[string]interface{}) {
 	stats = make(map[string]interface{})
 	if c.proc != nil {
 		delta := time.Now().Sub(c.lastT)
@@ -37,18 +41,22 @@ func (c *Counter) procStats() (stats map[string]interface{}) {
 		stats["virt_mem"] = humanize.Bytes(uint64(s.VirtualMemory()))
 		c.lastS = &s
 	}
+
+	stats["sleep"] = time.Duration(c.sleep)
+	stats["throughput"] = strconv.FormatFloat(float64(c.i)/c.tickD.Seconds(), 'f', 1, 64) + " EPS"
 	return
 }
 
-func (c *Counter) Stats(n string) *Stats {
-	stats := &Stats{
-		Action: n,
-	}
-	stats.Expected, stats.Actual = c.statsByAction(n)
+func (c *Counter) logStats() {
+	c.log.Info("collecting...")
+	stats := c.globalStats()
+	logStatsEntry := c.log.WithFields(logger.Fields(stats))
 
-	if stats.Expected > 0 {
-		stats.Percentage = int(100 * stats.Actual / stats.Expected)
+	time.Sleep(time.Second)
+	for _, n := range c.actions {
+		s := c.statsByAction(n)
+		logEntry := c.log.WithField("expected", s.Expected).WithField("actual", s.Actual)
+		logEntry.Infof("%s (%d%% lost)", s.Action, 100-s.Percentage)
 	}
-
-	return stats
+	logStatsEntry.Info("statistics")
 }
