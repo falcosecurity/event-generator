971ca87402e49b1950fd8e5d247a8020d2e0c06c
Leonardo Grasso <me@leonardograsso.com>
2020-11-10 12:33:15 +0100

    new(cmd): add `--logformat` flag with json support
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- cmd/internal/validate/islogruslevel.go
+++ cmd/internal/validate/islogruslevel.go
@@ -7,10 +7,9 @@ import (
 
 func isLogrusLevel(fl validator.FieldLevel) bool {
 	level := fl.Field().String()
-	lvl, err := logger.ParseLevel(level)
+	_, err := logger.ParseLevel(level)
 	if err != nil {
 		return false
 	}
-	logger.SetLevel(lvl)
 	return true
 }
