57e411fe76bfb93f9484fd4dd3d0d5298f30b111
Leonardo Grasso <me@leonardograsso.com>
2021-07-07 14:43:43 +0200

    update(events/syscall): ChangeThreadNamespace disabled by default
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- events/syscall/change_thread_namespace.go
+++ events/syscall/change_thread_namespace.go
@@ -8,7 +8,10 @@ import (
 	"github.com/falcosecurity/event-generator/events"
 )
 
-var _ = events.Register(ChangeThreadNamespace)
+var _ = events.Register(
+	ChangeThreadNamespace,
+	events.WithDisabled(), // the rule is not enabled by default, so disable the action too
+)
 
 func ChangeThreadNamespace(h events.Helper) error {
 	// It doesn't matter that the arguments to Setns are
