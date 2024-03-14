e84065b963e63d8614044414f29d9ebee0216e23
h4l0gen <ks3913688@gmail.com>
2024-03-14 22:54:44 +0530

    adding an event of deleting bash history
    Signed-off-by: h4l0gen <ks3913688@gmail.com>

---
+++ events/syscall/delete_or_rename_shell_history.go
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: Apache-2.0
+/*
+Copyright (C) 2024 The Falco Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package syscall
+
+import (
+    "os"
+    "path/filepath"
+
+    "github.com/falcosecurity/event-generator/events"
+)
+
+var _ = events.Register(DeleteOrRenameShellHistory)
+
+func DeleteOrRenameShellHistory(h events.Helper) error {
+    homeDir, err := os.UserHomeDir()
+    if err != nil {
+        return err
+    }
+
+    historyFile := filepath.Join(homeDir, ".bash_history")
+
+    h.Log().Infof("Shell history deleted: %s", historyFile)
+    
+	err = os.Remove(historyFile)
+	if err!= nil {
+		return err
+	}
+
+	return nil
+}
