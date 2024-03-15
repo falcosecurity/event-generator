141a4556d73b0a6c74d92d7fc82dc7dfe817ef8d
GLVS Kiriti <glvskiriti2003369@gmail.com>
2024-03-15 13:16:27 +0530

    Added event for default rule read ssh information
    Signed-off-by: GLVS Kiriti <glvskiriti2003369@gmail.com>

---
+++ events/syscall/read_ssh_information.go
@@ -0,0 +1,37 @@
+//go:build linux
+// +build linux
+
+// SPDX-License-Identifier: Apache-2.0
+/*
+Copyright (C) 2023 The Falco Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package syscall
+
+import (
+	"github.com/falcosecurity/event-generator/events"
+
+	"os"
+)
+
+var _ = events.Register(
+	ReadSshInformation,
+	events.WithDisabled(), // the rule is not enabled by default, so disable the action too
+)
+
+func ReadSshInformation(h events.Helper) error {
+	const filename = "/root/.ssh/known_hosts"
+	h.Log().Info("attempting to simulate SSH information read")
+	file, err := os.Open(filename)
+	defer file.Close()
+	return err
+}
