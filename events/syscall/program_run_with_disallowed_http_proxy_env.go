10c18a345aadf9585c6e70f5d3dffa7826180b98
GLVS Kiriti <glvskiriti2003369@gmail.com>
2024-03-16 17:23:53 +0530

    Added an event for default rule program run with diallowed http proxy env
    Signed-off-by: GLVS Kiriti <glvskiriti2003369@gmail.com>

---
+++ events/syscall/program_run_with_disallowed_http_proxy_env.go
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: Apache-2.0
+/*
+Copyright (C) 2024 The Falco Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package syscall
+
+import (
+	"os"
+	"os/exec"
+
+	"github.com/falcosecurity/event-generator/events"
+)
+
+var _ = events.Register(
+	ProgramRunWithDisallowedHttpProxyEnv,
+	events.WithDisabled(), // this rules is not included in falco_rules.yaml (stable rules), so disable the action
+)
+
+func ProgramRunWithDisallowedHttpProxyEnv(h events.Helper) error {
+	h.Log().Info("executing curl or wget with disallowed HTTP_PROXY environment variable")
+
+	os.Setenv("HTTP_PROXY", "http://my.http.proxy.com ")
+	cmd := exec.Command("curl", "http://example.com")
+
+	return cmd.Run()
+}
