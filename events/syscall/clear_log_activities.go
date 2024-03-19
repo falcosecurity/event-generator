c04f57df909991c40d81b69b760146d0a005281d
GLVS Kiriti <glvskiriti2003369@gmail.com>
2024-03-19 13:41:21 +0530

    Added an event for default stable rule clear log activities
    Signed-off-by: GLVS Kiriti <glvskiriti2003369@gmail.com>

---
+++ events/syscall/clear_log_activities.go
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: Apache-2.0
+/*
+Copyright (C) 2024 The Falco Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package syscall
+
+import (
+	"os"
+
+	"github.com/falcosecurity/event-generator/events"
+)
+
+var _ = events.Register(ClearLogActivites)
+
+func ClearLogActivites(h events.Helper) error {
+	// Opening a file from access log files list with O_TRUNC flag
+	filename := "/var/log/syslog"
+	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		h.Log().WithError(err).Error("Error opening file")
+		return err
+	}
+	defer file.Close()
+
+	// Perform some write operation on the file
+	_, err = file.WriteString("Created by falco event generator")
+	if err != nil {
+		h.Log().WithError(err).Error("Error writing into file")
+		return err
+	}
+
+	h.Log().Info("Log file: ", filename, "was tampered.")
+	return nil
+}
