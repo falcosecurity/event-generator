ebc6f6ceaf2f3c8278d1461509bd5b40b7c0e88a
h4l0gen <ks3913688@gmail.com>
2024-03-21 16:39:25 +0530

    adding an event on modify container entrypoint
    Signed-off-by: h4l0gen <ks3913688@gmail.com>
    
    squashed commits
    
    Signed-off-by: h4l0gen <ks3913688@gmail.com>

---
+++ events/syscall/modify_container_entrypoint.go
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: Apache-2.0
+/*
+Copyright (C) 2024 The Falco Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package syscall
+
+import (
+	"github.com/falcosecurity/event-generator/events"
+	"io"
+	"os"
+)
+
+var _ = events.Register(
+	ModifyContainerEntrypoint,
+	events.WithDisabled(), // this rules is not included in falco_rules.yaml (stable rules), so disable the action
+)
+
+func ModifyContainerEntrypoint(h events.Helper) error {
+	if h.InContainer() {
+		file, err := os.OpenFile("/proc/self/fd/1", os.O_WRONLY, 0644)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		// Get the current size of the file
+		fileInfo, err := file.Stat()
+		if err != nil {
+			return err
+		}
+		initialFileSize := fileInfo.Size()
+
+		// Write "written by event-generator" to the end of the file
+		data := []byte("written by event-generator")
+
+		_, err = file.Seek(0, io.SeekEnd)
+		if err != nil {
+			return err
+		}
+		_, err = file.Write(data)
+		if err != nil {
+			return err
+		}
+		// Truncate the file to its initial size to remove the written content
+		err = file.Truncate(initialFileSize)
+		if err != nil {
+			return err
+		}
+	}
+	return &events.ErrSkipped{
+		Reason: "'Modify Container Entrypoint' is applicable only to containers.",
+	}
+}
