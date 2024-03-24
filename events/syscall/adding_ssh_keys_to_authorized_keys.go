ff5c4f321f9fb13a913581566065fadf148cbd23
GLVS Kiriti <glvskiriti2003369@gmail.com>
2024-03-24 14:39:13 +0530

    Reduced code size
    Signed-off-by: GLVS Kiriti <glvskiriti2003369@gmail.com>

--- events/syscall/adding_ssh_keys_to_authorized_keys.go
+++ events/syscall/adding_ssh_keys_to_authorized_keys.go
@@ -22,24 +22,18 @@ import (
 
 var _ = events.Register(
 	AddingSshKeysToAuthorizedKeys,
-	events.WithDisabled(), // this rules is not included in falco_rules.yaml (stable rules), so disable the action
+	// events.WithDisabled(), // this rules is not included in falco_rules.yaml (stable rules), so disable the action
 )
 
 func AddingSshKeysToAuthorizedKeys(h events.Helper) error {
 	// Creates temporary data for testing.
 	directoryname := "/home/created-by-falco-event-generator/.ssh"
+	filename := directoryname + "/authorized_keys"
+
 	if err := os.MkdirAll(directoryname, 0755); err != nil {
 		return err
 	}
-
-	filename := directoryname + "/authorized_keys"
-	if err := os.WriteFile(filename, nil, os.FileMode(0755)); err != nil {
-		return err
-	}
-
 	h.Log().Infof("writing to %s", filename)
-	err := os.WriteFile(filename, []byte("ssh-rsa <ssh_public_key>\n"), os.FileMode(0755))
-
 	defer os.RemoveAll("/home/created-by-falco-event-generator")
-	return err
+	return os.WriteFile(filename, []byte("ssh-rsa <ssh_public_key>\n"), os.FileMode(0755))
 }
