e2d7d250033de377d30db8d3c5290ea6a51c405b
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2024-10-22 09:06:49 +0200

    feat(decl/syscalls): add open system call test step
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
    Co-authored-by: Aldo Lacuku <aldo@lacuku.eu>

--- pkg/test/step/syscall/base/parse.go
+++ pkg/test/step/syscall/base/parse.go
@@ -18,6 +18,7 @@ package base
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 func parseFD(value string) (int, error) {
@@ -40,4 +41,48 @@ func parseBufferLen(value string) (int, error) {
 	}
 
 	return int(bufferLen), nil
+}
+
+func parseFilePath(value string) ([]byte, error) {
+	if strings.IndexByte(value, 0) != -1 {
+		return nil, fmt.Errorf("unexpected NULL byte")
+	}
+
+	filePath := make([]byte, len(value)+1)
+	copy(filePath, value)
+	return filePath, nil
+}
+
+func parseOpenFlags(value string) (int, error) {
+	if flags, err := strconv.ParseInt(value, 10, 0); err == nil {
+		return int(flags), nil
+	}
+
+	flags := 0
+	for _, flag := range strings.Split(value, "|") {
+		flagValue, ok := openFlags[flag]
+		if !ok {
+			return 0, fmt.Errorf("unknown flag %q", flag)
+		}
+		flags |= flagValue
+	}
+
+	return flags, nil
+}
+
+func parseOpenMode(value string) (int, error) {
+	if flags, err := strconv.ParseInt(value, 10, 0); err == nil {
+		return int(flags), nil
+	}
+
+	flags := 0
+	for _, flag := range strings.Split(value, "|") {
+		flagValue, ok := openModes[flag]
+		if !ok {
+			return 0, fmt.Errorf("unknown mode %q", flag)
+		}
+		flags |= flagValue
+	}
+
+	return flags, nil
 }
