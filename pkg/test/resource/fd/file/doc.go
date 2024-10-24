eea8d60ffb6d974a780b2f4ec1809c1e8ac09932
Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
2024-10-24 16:59:42 +0200

    feat(decl/fd): add file fd test resource
    Signed-off-by: Leonardo Di Giovanna <leonardodigiovanna1@gmail.com>
    Co-authored-by: Aldo Lacuku <aldo@lacuku.eu>

---
+++ pkg/test/resource/fd/file/doc.go
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2024 The Falco Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package file provides the implementation of a regular file fd test resource. The resource exposes a 'fd' field,
+// referring to a regular file, that can be used in field binding.
+package file
